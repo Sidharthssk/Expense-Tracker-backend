00e74bfde9020474999ec2c53fa5713aecaed67f
Sidharthssk <sidharthssk.18@gmail.com>
2022-10-15 13:34:22 +0530

    Created route for editting daily expense

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const port = 8000
 app.use(express.json());
 
 
+
+
 // Available Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/expense', require('./routes/dailyExpense'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
 })
 
 app.listen(port, () => {
--- routes/dailyExpense.js
+++ routes/dailyExpense.js
@@ -34,4 +34,27 @@ async (req, res) =>{
     }
 })
 
+router.post('/editExpense/:id',fetchuser,
+async (req, res)=>{
+    let {expense_tag, amount} = req.body;
+
+    const newExpenseObj = new Expense({
+        expense_tag: expense_tag,
+        amount: amount
+    });
+
+    let dailyexpenseObj = await Dailyexpense.findById(req.params.id);
+
+    if(!dailyexpenseObj){
+        return res.status(400).send("Not found");
+    }
+
+    if(dailyexpenseObj.user.toString() !== req.user.id){
+        return res.status(401).send("Not allowed");
+    }
+
+    dailyexpenseObj = await Dailyexpense.findByIdAndUpdate(req.params.id, {expenses: newExpenseObj}, {new: true});
+    res.json({dailyexpenseObj});
+})
+
 module.exports = router;
