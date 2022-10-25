757af8f25b770fd07e8675e3f0e86802fe83012e
Sidharthssk <sidharthssk.18@gmail.com>
2022-10-25 20:01:01 +0530

    changed mongoDB port

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 connectToMongo().catch(err => console.log(err));
 
 async function connectToMongo() {
-  await mongoose.connect('mongodb://localhost:27017/expenseTracker?directConnection=true');
+  await mongoose.connect('mongodb+srv://sidharthssk18:Captain2002@expense-tracker.jwqxgv5.mongodb.net/?retryWrites=true&w=majority');
   console.log("Connection successful");
 }
 
