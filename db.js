b23b21ec99fa9da8b074343d5b95a2fcc99e7c42
Sidharthssk <sidharthssk.18@gmail.com>
2022-11-02 12:07:02 +0530

    made some fixes

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 connectToMongo().catch(err => console.log(err));
 
 async function connectToMongo() {
-  await mongoose.connect('mongodb+srv://sidharthssk18:Captain2002@expense-tracker.jwqxgv5.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://sidharth:sidharth123@cluster0.cwuktn6.mongodb.net/?retryWrites=true&w=majority');
   console.log("Connection successful");
 }
 
